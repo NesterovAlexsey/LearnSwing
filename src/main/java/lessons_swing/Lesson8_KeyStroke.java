217d533bff46ef2d434ac8170e06475683ce8195
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2024-05-21 05:49:48 +0200

    Lesson 8 start - KeyStroke

---
+++ src/main/java/lessons_swing/Lesson8_KeyStroke.java
@@ -0,0 +1,49 @@
+package lessons_swing;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import lessons_swing.Lesson7_Action.MyAction;
+
+public class Lesson8_KeyStroke {
+
+  //статический метод, сразу создает нужный фрейм через объявление переменной!
+  static JFrame jFrame = getFrame();
+  static JPanel jPanel = new JPanel();
+
+  public static void main(String[] args) {
+    jFrame.add(jPanel);
+    AbstractAction myAction = new MyAction();
+    //кнока создаеться сразу с наследованием экшена!
+    JButton jButton = new JButton(myAction);
+    jButton.setText("Test");
+    jPanel.add(jButton);
+  }
+
+  static class MyAction extends AbstractAction {
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+      jPanel.setBackground(Color.BLUE);
+    }
+  }
+  static JFrame getFrame() {
+    var jFrame = new JFrame() {};
+    jFrame.setVisible(true);
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jFrame.setTitle("My First App");
+
+    //задание нужного размера экрана относительно размера кнопки
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    Dimension dimension = toolkit.getScreenSize();
+    jFrame.setBounds(dimension.width/4, dimension.height/4, 500, 300);
+
+    return jFrame;
+  }
+
+}
