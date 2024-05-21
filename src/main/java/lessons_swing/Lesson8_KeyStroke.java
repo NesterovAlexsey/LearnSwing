ba9888d1343b62625b905606c8c239fcd3b78b3f
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2024-05-21 06:22:34 +0200

    Lesson 8 - create hot keys

--- src/main/java/lessons_swing/Lesson8_KeyStroke.java
+++ src/main/java/lessons_swing/Lesson8_KeyStroke.java
@@ -5,10 +5,13 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import lessons_swing.Lesson7_Action.MyAction;
+import javax.swing.KeyStroke;
 
 public class Lesson8_KeyStroke {
 
@@ -23,6 +26,21 @@ public class Lesson8_KeyStroke {
     JButton jButton = new JButton(myAction);
     jButton.setText("Test");
     jPanel.add(jButton);
+
+    //для работы горячих клавиш окно должно быть в фокусе
+    jPanel.setFocusable(true);
+    jPanel.requestFocusInWindow();
+
+    //создание горячих клавиш
+    KeyStroke keyStroke = KeyStroke.getKeyStroke("ctrl B");
+    //где горячие клавиши сработают: окно, кнопка, пр.
+    InputMap inputMap = jPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    //задаем название действие, с которым связана горячая клавиша
+    inputMap.put(keyStroke, "changeColor");
+    //?
+    ActionMap actionMap = jPanel.getActionMap();
+    //связываем инпут мап с готовой клавишей с конкретным экшоном
+    actionMap.put("changeColor", myAction);
   }
 
   static class MyAction extends AbstractAction {
